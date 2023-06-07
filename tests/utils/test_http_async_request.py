32c78738ed4ef696b0f0be90bfd4fdbba79968a2
Pedro Mayorga <ppmayorga80@gmail.com>
2023-06-07 02:27:38 -0600

    first version of http_async_request #MINOR_VERSION

--- tests/utils/test_http_async_request.py
+++ tests/utils/test_http_async_request.py
@@ -1,157 +1,204 @@
 """test for http async request file"""
-import re
-from unittest.mock import patch, Mock
+from unittest.mock import Mock
 
-from computing_toolbox.utils.http_async_request import HttpAsyncRequest
+from aioresponses import aioresponses
+
+from computing_toolbox.utils.http_async_request import HttpAsyncResponse, HttpAsyncRequest
 
 
-def create_mock_response(status_code: int = 200):
+def create_mock_client_response(status: int or None = 200):
     mock_response = Mock()
-    mock_response.status_code = status_code
+    mock_response.status = status
     return mock_response
 
 
-class TestHttpAsyncRequest:
-    """test class for HttpAsyncRequest"""
-    MOCK_200_RESPONSE = create_mock_response(200)
-    MOCK_201_RESPONSE = create_mock_response(201)
-    MOCK_202_RESPONSE = create_mock_response(202)
-    MOCK_404_RESPONSE = create_mock_response(404)
-    MOCK_NONE_RESPONSE = None
+class TestHttpAsyncResponse:
+    """test for a http response class"""
 
-    def test_init_str_fix_params(self):
-        """test 3 methods"""
-        # 1. test constructor
+    def test_set(self):
+        """test set method"""
+        response_ok = create_mock_client_response(200)
+        response_fail = create_mock_client_response(404)
+        value_error = ValueError("some error")
+
+        response = HttpAsyncResponse()
+
+        # test if set to success
+        response.set(response_ok, "hello, world")
+        assert response.success
+        assert response.attempts == 1
+        assert response.text == "hello, world"
+        assert response.response_history[-1] is not None
+        assert response.error_history[-1] is None
+
+        # test for a failure
+        response.set(response_fail, "")
+        assert not response.success
+        assert response.attempts == 2
+        assert not response.text
+        assert response.response_history[-1] is not None
+        assert response.error_history[-1] is None
+
+        # test for an error
+        response.set_error(value_error)
+        assert not response.success
+        assert response.attempts == 3
+        assert not response.text
+        assert response.response_history[-1] is None
+        assert response.error_history[-1] is not None
+
+
+class TestHttpAsyncRequest:
+    """class for testing http async request"""
+
+    def test_init(self):
+        """test the constructor"""
         requester = HttpAsyncRequest()
-        assert requester.max_attempts == 10
+        assert requester.max_attempts > 0
         assert requester.rnd_sleep_interval is None
         assert not requester.urls
         assert not requester.method
-        assert not requester.responses
         assert not requester.responses_history
-        assert not requester.success
-        assert not requester.attempts
-        assert requester.execution_time < 0
+        assert not requester.progress_bar
 
-        # 2, test magic method __str__
-        text = str(requester)
-        assert re.findall(
-            r"HttpAsyncRequest\(\'[^()]*\'\).*status_codes:.*AVG\(attempts\):.*TOTAL\(attempts\):.*Elapsed time:.*",
-            text)
-
-        # 3. test _fix_params method
-        #    even we use all defaults (None values)
-        #    we get fixed values
-        input_urls = ["/my/page/1", "/my/page/2"]
-        urls, params, datas, headers, timeout, allow_redirects, proxies, request_kwargs, tqdm_kwargs = requester._fix_params(
-            input_urls)
-        assert urls == input_urls
-        assert isinstance(params, list) and len(params) == len(urls)
-        assert isinstance(datas, list) and len(datas) == len(urls)
-        assert isinstance(headers, list) and len(headers) == len(urls)
-        assert isinstance(timeout, list) and len(timeout) == len(urls)
-        assert isinstance(allow_redirects,
-                          list) and len(allow_redirects) == len(urls)
-        assert isinstance(proxies, list) and len(proxies) == len(urls)
-        assert isinstance(request_kwargs,
-                          list) and len(request_kwargs) == len(urls)
-        assert tqdm_kwargs is None  # not default tqdm_kwargs
-
-    @patch("computing_toolbox.utils.http_async_request.grequests")
-    def test_request_no_retries(self, mock_grequests):
-        """test the async http request with no retries i.e.
-        all requests are completed at first attempt
-        """
-        # 1. define the urls to test
-        my_urls = ["/page/1", "other/page", "my/third/page"]
-
-        # 2. set mocking values
-        mock_success_response = create_mock_response(200)
-        mock_grequests.request.return_value = None
-        mock_grequests.map.return_value = [
-            mock_success_response for _ in range(len(my_urls))
-        ]
-
-        # execute the async request and test
+    def test_magic_str(self):
+        """test the magic string method"""
         requester = HttpAsyncRequest()
-        responses = requester.request("GET", my_urls)
-        assert len(responses) == len(my_urls)
-        assert all(r.status_code == 200 for r in responses)
+        requester_str = str(requester)
+        assert "HttpAsyncRequest" in requester_str
 
-    @patch("computing_toolbox.utils.http_async_request.grequests")
-    def test_request_with_bad_responses(self, mock_grequests):
-        """test async http request with 4 attempts out of 5.
-        the 4th attempt complete the request with all success
-        """
-        # 1. define the urls to test
-        my_urls = [
-            "http://www.mypage.com/1", "http://www.mypage.com/2",
-            "http://www.mypage.com/3"
-        ]
+    def test_summary(self):
+        """test summary function"""
 
-        # 2. set mocking values
-        mock_grequests.request.return_value = "not matter"
-        # setting responses:
-        # 1st attempt: [200,404,404] #out first url
-        # 2nd attempt: [None,202] # out third url
-        # 3th attempt: [404]
-        # 4th attempt: [200]
-        expected_response_list = [[
-            self.MOCK_200_RESPONSE, self.MOCK_404_RESPONSE,
-            self.MOCK_404_RESPONSE
-        ], [self.MOCK_NONE_RESPONSE, self.MOCK_202_RESPONSE],
-                                  [self.MOCK_404_RESPONSE],
-                                  [self.MOCK_201_RESPONSE]]
-        mock_grequests.map.side_effect = expected_response_list
+        # 1. add some values to simulate results
+        response_ok = create_mock_client_response(200)
+        response_fail = create_mock_client_response(404)
+        response_error = create_mock_client_response(None)
+        value_error = ValueError("some error")
 
-        # execute the async request and test
-        requester = HttpAsyncRequest(max_attempts=5,
-                                     rnd_sleep_interval=(0.001, 0.002))
-        responses = requester.request("GET", my_urls, tqdm_kwargs={})
-        assert len(responses) == len(my_urls)
-        assert responses[0].status_code == 200
-        assert responses[1].status_code == 201
-        assert responses[2].status_code == 202
-        assert sum(requester.attempts) == sum(
-            len(x) for x in expected_response_list)
+        resp1 = HttpAsyncResponse(flag="",
+                                  response=response_ok,
+                                  status_code=200,
+                                  success=True,
+                                  text="hello, world",
+                                  attempts=1,
+                                  response_history=[response_ok],
+                                  error_history=[None])
+        resp2 = HttpAsyncResponse(
+            flag="",
+            response=response_fail,
+            status_code=404,
+            success=False,
+            text="",
+            attempts=2,
+            response_history=[response_fail, response_fail],
+            error_history=[None, None])
+        resp3 = HttpAsyncResponse(
+            flag="",
+            response=response_error,
+            status_code=None,
+            success=False,
+            text="",
+            attempts=2,
+            response_history=[response_fail, response_error],
+            error_history=[None, value_error])
 
-    @patch("computing_toolbox.utils.http_async_request.grequests")
-    def test_request_with_one_failed(self, mock_grequests):
-        """test async request with one failed
-        configure the mock request in order to
-        success with 1st and 3rd url but the second one
-        fails up to the end.
-        """
-        # 1. define the urls to test
-        my_urls = [
-            "http://www.other.com/1", "http://www.other.com/2",
-            "http://www.other.com/3"
-        ]
+        # 2. create the requester
+        requester = HttpAsyncRequest()
 
-        # 2. set mocking values
-        mock_grequests.request.return_value = None
-        # setting responses:
-        # 1st attempt: [200,404,404] #out first url
-        # 2nd attempt: [404,202] # out third url
-        # 3th attempt: [404]
-        # 4th attempt: [404]
-        # 5th attempt: [404] #this make 2nd url isn't success
-        expected_response_list = [[
-            self.MOCK_200_RESPONSE, self.MOCK_404_RESPONSE,
-            self.MOCK_404_RESPONSE
-        ], [self.MOCK_404_RESPONSE, self.MOCK_202_RESPONSE],
-                                  [self.MOCK_404_RESPONSE],
-                                  [self.MOCK_404_RESPONSE],
-                                  [self.MOCK_404_RESPONSE]]
-        mock_grequests.map.side_effect = expected_response_list
+        requester.method = "GET"
+        requester.urls = ["a", "b", "c"]
+        requester.responses_history = [resp1, resp2, resp3]
 
-        # execute the async request and test
-        requester = HttpAsyncRequest(max_attempts=5,
-                                     rnd_sleep_interval=(0.001, 0.002))
-        responses = requester.request("GET", my_urls, tqdm_kwargs={})
-        assert len(responses) == len(my_urls)
-        assert responses[0].status_code == 200
-        assert responses[1].status_code == 404
-        assert responses[2].status_code == 202
-        assert sum(requester.attempts) == sum(
-            len(x) for x in expected_response_list)
+        summary1 = requester.summary()
+        summary2 = requester.summary(exclude_success=True,
+                                     exclude_failures=True,
+                                     exclude_errors=True)
+        assert len(summary1) == 3
+        assert not summary2
+
+        assert sum(1 for x in summary1
+                   if x["status_code"] and x["status_code"] == 200) == 1
+        assert sum(1 for x in summary1
+                   if x["status_code"] and x["status_code"] == 404) == 1
+        assert sum(1 for x in summary1 if x["status_code"] is None) == 1
+
+    def test_request_v1(self):
+        """test the request with default values"""
+
+        urls = ["/url/1", "/url/2", "/url/3"]
+
+        # B. define an example with retries
+        with aioresponses() as m:
+            # B.1 define the results for the urls (two times to match the number of retries)
+            m.get(urls[0], payload="hello, world from page 1")
+            m.get(urls[1], payload="hello, world from page 2")
+            m.get(urls[2], payload="hello, world from page 3")
+
+            requester = HttpAsyncRequest()
+            requester.request("GET", urls)
+            assert isinstance(requester.responses_history, list)
+            assert len(requester.responses_history) == 3
+            assert sum(1 for r in requester.responses_history
+                       if r.status_code == 200) == 3
+
+    def test_request_v1_with_tqdm(self):
+        """test the request with default values"""
+
+        urls = ["/url/1", "/url/2", "/url/3"]
+
+        # B. define an example with retries
+        with aioresponses() as m:
+            # B.1 define the results for the urls (two times to match the number of retries)
+            m.get(urls[0], payload="hello, world from page 1")
+            m.get(urls[1], status=404)
+            m.get(urls[1], status=404)
+            m.get(urls[2], status=404)  # second will be an error
+
+            requester = HttpAsyncRequest(max_attempts=2)
+            requester.request("GET", urls, tqdm_kwargs={})
+
+            assert isinstance(requester.responses_history, list)
+            assert len(requester.responses_history) == 3
+            assert sum(1 for r in requester.responses_history
+                       if r.status_code == 200) == 1
+            assert sum(1 for r in requester.responses_history
+                       if r.status_code == 404) == 1
+            assert sum(1 for r in requester.responses_history
+                       if r.status_code is None) == 1
+
+    def test_request_with_list_parameters(self):
+        """test with predefined values"""
+
+        urls = ["/url/1", "/url/2", "/url/3"]
+
+        # B. define an example with retries
+        with aioresponses() as m:
+            # B.1 define the results for the urls (two times to match the number of retries)
+            m.get(urls[0], payload="hello, world from page 1")
+            m.get(urls[1], payload="hello, world from page 1")
+            m.get(urls[2], payload="hello, world from page 1")
+
+            requester = HttpAsyncRequest(max_attempts=2)
+            requester.request("GET", urls, timeout=[3, 4, 5], tqdm_kwargs={})
+
+            assert isinstance(requester.responses_history, list)
+
+    def test_request_with_random_sleep_interval(self):
+        """test with predefined values"""
+
+        urls = ["/url/1", "/url/2", "/url/3"]
+
+        # B. define an example with retries
+        with aioresponses() as m:
+            # B.1 define the results for the urls (two times to match the number of retries)
+            m.get(urls[0], status=404)
+            m.get(urls[0], payload="hello, world from page 1")
+            m.get(urls[1], payload="hello, world from page 1")
+            m.get(urls[2], payload="hello, world from page 1")
+
+            requester = HttpAsyncRequest(max_attempts=2,
+                                         rnd_sleep_interval=(0.001, 0.002))
+            requester.request("GET", urls, timeout=[3, 4, 5], tqdm_kwargs={})
+
+            assert isinstance(requester.responses_history, list)
