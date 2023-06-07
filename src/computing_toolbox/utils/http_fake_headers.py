dbadd57b769abd0dd3d64145b745c09ec08de81c
pedro <131192149+ppmayorga80@users.noreply.github.com>
2023-06-07 02:35:02 -0600

    Change grequests to aiohttp (#2)
    * first commit
    
    * first commit
    
    * first version of async http
    
    * first version of http_async_request #MINOR_VERSION

--- src/computing_toolbox/utils/http_fake_headers.py
+++ src/computing_toolbox/utils/http_fake_headers.py
@@ -1,6 +1,31 @@
 """generate fake or random http headers"""
 from fake_headers import Headers
 
+DEFAULT_HEADERS = {
+    'accept':
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language':
+    'en',
+    'sec-ch-ua':
+    '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'sec-ch-ua-mobile':
+    '?0',
+    'sec-ch-ua-platform':
+    '"macOS"',
+    'sec-fetch-dest':
+    'document',
+    'sec-fetch-mode':
+    'navigate',
+    'sec-fetch-site':
+    'none',
+    'sec-fetch-user':
+    '?1',
+    'upgrade-insecure-requests':
+    '1',
+    'user-agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+}
+
 
 def generate_fake_headers(authority: str = "",
                           method: str = "get",
@@ -22,28 +47,11 @@ def generate_fake_headers(authority: str = "",
         "method": method,
         "scheme": scheme,
     }
-    #1.2 default headers taken from a real browser or take the default_header from arguments
-    default_headers = {
-        "accept":
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding":
-        "gzip, deflate, br",
-        "accept-language":
-        "en-US,en;q=0.9,es;q=0.8",
-        "cache-control":
-        "max-age=0",
-        "sec-fetch-dest":
-        "document",
-        "sec-fetch-mode":
-        "navigate",
-        "sec-fetch-site":
-        "same-origin",
-        "user-agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
-    } if default_headers is None else default_headers
-    #1.3 merge regular headers and default headers
+    # 1.2 default headers taken from a real browser or take the default_header from arguments
+    default_headers = DEFAULT_HEADERS if default_headers is None else default_headers
+    # 1.3 merge regular headers and default headers
     default_headers = {**regular_headers, **default_headers}
-    #1.4 make all keys lowercase
+    # 1.4 make all keys lowercase
     default_headers = {k.lower(): v for k, v in default_headers.items()}
 
     # 2. define a fake-header object and generate a random headers
